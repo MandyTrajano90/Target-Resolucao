27308ba4e691b7b24f8db4e7419b8fcc1336719b
Mandytrajano90 <mandytrajano90@gmail.com>
2025-01-16 13:17:48 -0300

    string invertida

---
+++ src/stringInvertida.py
@@ -0,0 +1,13 @@
+# 5 - String invertida
+
+entrada = input("Digite uma string para inverter: ")
+
+def inverter_string(s):
+    string_invertida = ""
+    for i in range(len(s) - 1, -1, -1):  
+        string_invertida += s[i]  
+    return string_invertida
+
+resultado = inverter_string(entrada)
+
+print("String invertida:", resultado)
